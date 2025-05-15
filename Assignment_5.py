d4645f9b6f5a8114340b5e93b0eb3734459e3e4d
aniket-arya-glitch <aryaaniket171@gmail.com>
2025-05-15 22:46:27 +0530

    Update Assignment_5.py

--- Assignment_5.py
+++ Assignment_5.py
@@ -1,16 +1,13 @@
 #Create a Dictionary of Student Marks
 
 names={'Alice':82, 'Max':79, 'Tennyson':50, 'Gwen':96, 'Aniket':70, 'Krishanth':99, 'Robert':100}
-names2={'alice':82, 'max':79, 'tennyson':50, 'gwen':96, 'aniket':70, 'krishanth':99, 'robert':100}
-n= input ("Enter student's name:")
+n= input ("Enter student's name:").capitalize()
 if (n in names):
     print ('Student name is:',n)
     print (n,'marks=',names[n])
-elif (n in names2):
-    print('Student name is:',n)
-    print(n, 'marks=', names2[n])
 else:
     print('Student not found!')
+
 
 
 
